e2c9451a09f9fb1e0aa2a95fbaf4fc1562f06693
Mike Roberts <mike@symphonia.io>
2020-09-17 11:45:18 -0400

    E5-4: Enable integration tests in CI automation

--- automation/deploy-ci.sh
+++ automation/deploy-ci.sh
@@ -2,12 +2,17 @@
 
 set -euo pipefail
 
+STACK_NAME=coffee-store-ci
+
+CLOUDFORMATION_ARTIFACTS_BUCKET=$(aws cloudformation list-exports --query "Exports[?Name==\`CloudformationArtifactsBucket\`].Value" --output text)
+if [ -z "$CLOUDFORMATION_ARTIFACTS_BUCKET" ]; then
+  echo "Unable to locate Cloudformation Export 'CloudformationArtifactsBucket' - have you setup the account-wide-resources for this account and region?"
+  exit 1
+fi
+
 sam deploy \
-        --stack-name coffee-store-ci \
+        --stack-name $STACK_NAME \
         --template-file ci.yaml \
         --capabilities CAPABILITY_IAM \
+        --parameter-overrides CloudformationArtifactsBucket=$CLOUDFORMATION_ARTIFACTS_BUCKET \
         --no-fail-on-empty-changeset
-
-echo
-echo Stack deployed as coffee-store-ci
-echo
